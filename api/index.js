86744de29da83bd4a094dc5d0410e6e204a63d47
Aditya-Bojja <baditya2070@gmail.com>
2024-11-14 15:49:23 +0530

    setup and connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_CONNECTION_STRING)
+    .then(() => {
+        console.log("Connected to MongoDB Cluster...");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
